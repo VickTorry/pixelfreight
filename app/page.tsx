245d499b0a7f8007a85d61b659b6ad035edfeff8
VickTorry <victoriadudnyk@gmail.com>
2025-07-21 22:35:01 +0200

    site under construction page

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,26 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import ContactForm from "@/components/LeaveYourEmailForm/LeaveYourEmailForm";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        <div className={styles.title}>
+  <Image
+    className={styles.logo}
+    src="/pflogo.svg"
+    alt="PixelFreight logo"
+    width={84}
+    height={84}
+    priority
+  />
+  <h1>PixelFreight is Coming Soon</h1>
+</div>
+      <p className={styles.description}>We&apos;re building something awesome. Leave your email to stay updated!</p>
+      <ContactForm />
       </main>
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           target="_blank"
@@ -89,7 +63,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
