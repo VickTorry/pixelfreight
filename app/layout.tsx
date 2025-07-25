8a84f00bbd99dd693391754a5597bb169de29feb
VickTorry <victoriadudnyk@gmail.com>
2025-07-25 16:24:09 +0200

    changed img to png for telegram

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     siteName: "Pixelfreight",
     images: [
       {
-        url: "underconstruction-img.webp", 
+        url: "underconstruction-img (10).png", 
         width: 1200,
         height: 630,
         alt: "Pixelfreight under construction",
