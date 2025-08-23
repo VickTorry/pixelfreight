c3359976d3799a4c3fb5b5b535795f94ef4c1a01
VickTorry <victoriadudnyk@gmail.com>
2025-08-23 19:27:20 +0200

    fixed analytics code

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-VGSLZ7BMLN');
+            gtag('config', 'G-VGSZL7BMLN');
           `}
         </Script>
 
